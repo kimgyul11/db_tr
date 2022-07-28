145b7962254232a4e938b2db4e95c05f720da9ec
kimgyul11 <coloo88@naver.com>
2022-07-28 14:16:28 +0900

    c

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,2 +1,17 @@
 -- suwon을 사용 하겠다. 상단에서 한번만 사용해
 use suwon;
+
+
+-- 전체 컬럼 조회
+select * from member;
+
+-- 컬럼 추가
+ALTER TABLE member2 ADD COLUMN nickname VARCHAR(45);
+
+ALTER TABLE member2 ADD COLUMN nameEng VARCHAR(45) AFTER name;
+
+-- 컬럼 변경
+ALTER TABLE member2 MODIFY COLUMN nickname varchar(100);
+
+-- 컬럼 이름 변경
+ALTER TABLE member2 CHANGE COLUMN nickname nick varchar(45);
