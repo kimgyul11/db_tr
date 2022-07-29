a6b53057dc0dcc98145af02da41ad43855989792
kimgyul11 <coloo88@naver.com>
2022-07-29 14:56:46 +0900

    commit

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -21,8 +21,60 @@ ALTER TABLE member2 drop COLUMN nick;
 
 -- row 삭제 : DELETE사용
 
+DELETE FROM answer_comment
+WHERE seq=8;
+
 select *
-from question;
+from question
+WHERE seq='1';
+
+-- 7월 29일 DB수업
+
+-- 1.LIKE
+-- 검색창에 검색하는거 거의 LIKE 검색사용.
+SELECT*FROM member2
+WHERE 1=1
+AND name like 'KI%';
+-- AND name like '%ki' 앞에는 뭐가나와도 괜찮음.
+-- AND name like '%ki%'중간에만 ki.
+SELECT* FROM member2
+where 1=1
+AND nameEng like'%gi%'
+;
+-- 2.between 범위지정해준다..
+SELECT* FROM member2
+where 1=1
+-- AND seq = 0
+-- AND seq > 1
+AND seq >= 1
+-- AND seq between 0 and 1
+;
+-- 3.null값 조회하기
+SELECT* FROM member2
+where 1=1
+AND name = ""
+;
+
+
+
+
+
+
+
+
+INSERT INTO answer_comment(
+	answer_seq
+    ,user_ID
+    ,comment
+    ,commet_write_time
+)
+VALUES(
+	3
+	,"JPKIMGIYUL"
+    ,"YAMANASHI KOFU,HOTLENAME DANROKAN"
+    ,"2022-07-25"
+);
+
 
 
 	
